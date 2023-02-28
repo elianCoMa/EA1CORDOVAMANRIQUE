4f306d354efbd4c05baf7d54d683108c744076d5
elian <elian@LAPTOP-V79H8RNG>
2023-02-27 22:49:27 -0500

    correcion de cine y cines

--- CINESTAR_EA1/src/main/java/dao/CineDAO.java
+++ CINESTAR_EA1/src/main/java/dao/CineDAO.java
@@ -3,7 +3,7 @@ package dao;
 public class CineDAO {
 	db.Db db = new db.Db("CineStar");
 	
-	public String[][] getVerCines() {
+	public String[][] getCines() {
 		db.Sentencia("call sp_getCines()");
 		return db.getRegistros();
 	}
